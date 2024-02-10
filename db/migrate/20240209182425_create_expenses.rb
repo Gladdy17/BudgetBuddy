272f42d3521bb6b707fe733d22793dd5f6e30cb5
Gladdy17 <gladston.bosco@gmail.com>
2024-02-09 17:27:42 -0700

    first commit

---
+++ db/migrate/20240209182425_create_expenses.rb
@@ -0,0 +1,13 @@
+class CreateExpenses < ActiveRecord::Migration[7.1]
+  def change
+    create_table :expenses do |t|
+      t.string :name
+      t.date :date
+      t.float :amount
+      t.text :description
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
