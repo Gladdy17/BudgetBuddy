b205f641381b1e48257e7a76fa5a0c49d4fa0032
gladdy17 <gladston.bosco@gmail.com>
2024-02-10 08:47:36 -0700

    devise models added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -8,4 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "home#index"
+   devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 end
